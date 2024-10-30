1e17b5195c517601c5fe05fa933e02f705648049
soufiane999fijaoui <fijaoui916@gmail.com>
2024-10-30 01:58:27 +0100

    la premiere commit

---
+++ Frontend/front-end/src/App.js
@@ -0,0 +1,17 @@
+import Navbar from './components/navbar';
+import './App.css';
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <h1>E-dashboard</h1>
+      <Navbar/>
+      </BrowserRouter>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/front-end/src/components/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li> <Link to="/"> Products</Link> </li>
+                <li> <Link to="/add"> Add Products</Link> </li>
+                <li> <Link to="/update"> Update Products</Link> </li>
+                <li> <Link to="/logout"> Logout</Link> </li>
+                <li> <Link to="/profile"> Profile</Link> </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
